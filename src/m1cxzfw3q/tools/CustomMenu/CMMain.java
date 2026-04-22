4176f426169cf2133e3eca6d9f1012352205c628
m1cxzfw3q <mcxzfwq@outlook.com>
2026-04-23 00:58:27 +0800

    update

---
+++ src/m1cxzfw3q/tools/CustomMenu/CMMain.java
@@ -0,0 +1,14 @@
+package m1cxzfw3q.tools.CustomMenu;
+
+import mindustry.mod.Mod;
+
+public class CMMain extends Mod {
+    public CMMain() {
+
+    }
+
+    @Override
+    public void loadContent() {
+
+    }
+}
